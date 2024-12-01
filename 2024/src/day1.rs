0fd96212f6c5d3eb50e2be0573aaff1b5655bed3
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-12-01 17:15:01 +0100

    Improve day 1

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,7 +1,18 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<Vec<i32>> {
-    get_lists(input)
+    let mut columns = Vec::new();
+    for line in input.lines() {
+        for (i, x) in line.split_whitespace().enumerate() {
+            let num = x.parse::<i32>().unwrap();
+            if columns.len() <= i {
+                columns.push(Vec::new());
+            }
+            columns[i].push(num);
+        }
+    }
+    columns
 }
 
 #[aoc(day1, part1)]
@@ -18,16 +29,6 @@ pub fn solve_part2(input: &Vec<Vec<i32>>) -> i32 {
 
     calculate_similarity(&sorted_columns)
 }
-fn get_lists(input: &str) -> Vec<Vec<i32>> {
-    let mut columns = vec![Vec::new(), Vec::new()];
-    for line in input.lines() {
-        let numbers: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
-        for (i, &num) in numbers.iter().enumerate() {
-            columns[i].push(num);
-        }
-    }
-    columns
-}
 
 fn sort_columns(columns: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut sorted_columns = columns.clone();
@@ -39,13 +40,9 @@ fn sort_columns(columns: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 }
 
 fn substract_columns(columns: &Vec<Vec<i32>>) -> Vec<i32> {
-    // for each row, get the difference between all the columns
     let mut result = Vec::new();
     for i in 0..columns[0].len() {
-        let mut diff = columns[0][i];
-        for j in 1..columns.len() {
-            diff -= columns[j][i];
-        }
+        let diff = columns[0][i] - columns[1][i];
         result.push(diff.abs());
     }
 
@@ -53,44 +50,46 @@ fn substract_columns(columns: &Vec<Vec<i32>>) -> Vec<i32> {
 }
 
 fn calculate_similarity(sorted_columns: &Vec<Vec<i32>>) -> i32 {
-    let mut index_right = 0;
+    let freq_col0 = count_frequencies(&sorted_columns[0]);
+    let freq_col1 = count_frequencies(&sorted_columns[1]);
 
-    let mut last_look_for = 0;
-    let mut last_nb_similar_right;
-    let mut last_result = 0;
-
+    let mut i = 0;
+    let mut j = 0;
     let mut result = 0;
-
-    for i in 0..sorted_columns[0].len() {
-        if last_look_for == sorted_columns[0][i] {
-            result += last_result;
-            continue;
+    
+    while i < freq_col0.len() && j < freq_col1.len() {
+        if freq_col0[i].0 == freq_col1[j].0 {
+            let value = freq_col0[i].0;
+            let count0 = freq_col0[i].1;
+            let count1 = freq_col1[j].1;
+            result += value * (count0 as i32) * (count1 as i32);
+            i += 1;
+            j += 1;
+        } else if freq_col0[i].0 < freq_col1[j].0 {
+            i += 1;
+        } else {
+            j += 1;
         }
-        last_look_for = sorted_columns[0][i];
-        // look for the same element in the right column and count the number of similar elements
-        (last_nb_similar_right, index_right) = look_for_element(&sorted_columns[1], last_look_for, index_right);
-
-        last_result = last_look_for * last_nb_similar_right;
-        result += last_result;
     }
 
     result
 }
 
-fn look_for_element(sorted_column: &Vec<i32>, look_for: i32, index: i32) -> (i32, i32) {
-    let mut nb_similar = 0;
-    let mut index_end = 0;
+fn count_frequencies(sorted_list: &[i32]) -> Vec<(i32, usize)> {
+    let mut frequencies = Vec::new();
+    let mut current_value = sorted_list[0];
+    let mut count = 1;
 
-    for j in index as usize..sorted_column.len() {
-        if sorted_column[j] == look_for {
-            nb_similar += 1;
-            index_end = j;
-            continue;
-        }
-        if sorted_column[j] > look_for {
-            break;
+    for &value in &sorted_list[1..] {
+        if value == current_value {
+            count += 1;
+        } else {
+            frequencies.push((current_value, count));
+            current_value = value;
+            count = 1;
         }
     }
+    frequencies.push((current_value, count));
 
-    (nb_similar, index_end as i32)
+    frequencies
 }
