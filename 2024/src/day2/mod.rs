31fca1ce6b2bfc33393ee7ceafa1855e20e21f1e
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-12-02 23:47:23 +0100

    Optimize day 2 part 2

--- 2024/src/day2/mod.rs
+++ 2024/src/day2/mod.rs
@@ -14,7 +14,7 @@ pub fn part1(input: &str) -> u16 {
     let lines = parse_input_day2(input);
     let mut nb_safe = 0;
     for i in 0..lines.len() {
-        if is_safe(&lines[i],0) == 0 {
+        if is_safe(&lines[i]) == 0 {
             nb_safe += 1;
         }
     }
@@ -27,7 +27,7 @@ pub fn part2(input: &str) -> u16 {
     let mut nb_safe = 0;
     for i in 0..lines.len() {
         for j in 0..lines[i].len() {
-            if is_safe(&lines[i], j) == 0 {
+            if is_safe(&try_without_number(&lines[i], j)) == 0 {
                 nb_safe += 1;
                 break;
             }
@@ -46,38 +46,13 @@ pub fn try_without_number(line: &Vec<u8>, index: usize) -> Vec<u8> {
     new_line
 }
 
-pub fn is_safe(line: &Vec<u8>, ignore_index: usize) -> usize {
-
-    let mut increase_or_decrease= false;
-    let mut known = false;
-    let mut last = 0;
-
-    println!("{:?}", line);
+pub fn is_safe(line: &Vec<u8>) -> usize {
+    let increase_or_decrease = get_increase_or_decrease(line[1], line[0]);
     for i in 1..line.len() {
-        if i == 1 && ignore_index == 1 {
-            continue;
-        }
-        if i == line.len()-1 && ignore_index == line.len()-1 {
-            continue;
-        }
-        
-        if ignore_index == 0 && i+1 != ignore_index {
-            last = i-1;
-            println!("Last: {}", i);
-        }
-        println!("Last: {}, i : {}", last, i);
-        if !known {
-            increase_or_decrease = get_increase_or_decrease(line[i], line[last]);
-            println!("Increase or decrease: {}", increase_or_decrease);
-            known = true;
-        }
-
-        if line[last].abs_diff(line[i]) > 3 || line[i] == line[last] {
-            println!("UNSAFE Diff: {}", line[last].abs_diff(line[i]));
+        if line[i].abs_diff(line[i - 1]) > 3 || line[i] == line[i - 1] {
             return i;
         }
-        if increase_or_decrease != get_increase_or_decrease(line[i], line[last]) {
-            println!("UNSAFE Increase or decrease diff: {}", get_increase_or_decrease(line[i], line[last]));
+        if increase_or_decrease != get_increase_or_decrease(line[i], line[i - 1]) {
             return i;
         }
     }
